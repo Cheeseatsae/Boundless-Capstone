69d2b077788f7767d115fa74bf3c7a19e3c9cf84
harry <1011422@student.sae.edu.au>
2019-05-29 17:02:29 +1000

    First Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public KeyCode jump {get; set;}
+    public KeyCode forward {get; set;}
+    public KeyCode backward {get; set;}
+    public KeyCode left {get; set;}
+    public KeyCode right {get; set;}
+
+    public string sJump = "Space";
+    public string sForward = "W";
+    public string sBack = "S";
+    public string sLeft = "A";
+    public string sRight = "D";
+
+    public delegate void InputAction(float i);
+
+    public event InputAction OnJumpInput;
+    public event InputAction OnForwardInput;
+    public event InputAction OnBackwardInput;
+    public event InputAction OnLeftInput;
+    public event InputAction OnRightInput;
+    
+    private void Awake()
+    {
+        jump = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("jumpKey", sJump));
+        forward = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("forwardKey", sForward));
+        backward = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("backwardKey", sBack));
+        left = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("leftKey", sLeft));
+        right = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("rightKey", sRight));
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetKey(jump))
+            OnJumpInput?.Invoke(1);
+        else
+            OnJumpInput?.Invoke(0);
+        
+        if (Input.GetKey(forward)) 
+            OnForwardInput?.Invoke(1);
+        else 
+            OnForwardInput?.Invoke(0);
+        
+        if (Input.GetKey(backward)) 
+            OnBackwardInput?.Invoke(1);
+        else 
+            OnBackwardInput?.Invoke(0);
+        
+        if (Input.GetKey(left)) 
+            OnLeftInput?.Invoke(1);
+        else
+            OnLeftInput?.Invoke(0);
+        
+        if (Input.GetKey(right)) 
+            OnRightInput?.Invoke(1);
+        else
+            OnRightInput?.Invoke(0);
+        
+    }
+    
+}
---
+++ Assets/PlayerModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel : MonoBehaviour
+{
+    public float baseSpeed;
+    public float speed;
+    public float baseSprintSpeedMult;
+    public float sprintSpeedMult;
+    public int baseJumps;
+    public int jumps;
+    public float baseJumpHeight;
+    public float jumpHeight;
+
+    public Rigidbody body;
+    
+//    public float baseHealth;
+//    public float health;
+//    public float baseMaxHealth;
+//    public float maxHealth;
+//    public float baseHealthRegen;
+//    public float healthRegen;
+//
+//    public float baseAttackSpeed;
+//    public float attackSpeed;
+//    public float baseAttackRange;
+//    public float attackRange;
+//    public float baseAttackDamage;
+//    public float attackDamage;
+
+    private void Start()
+    {
+        speed = baseSpeed;
+        sprintSpeedMult = baseSprintSpeedMult;
+        jumps = baseJumps;
+        jumpHeight = baseJumpHeight;
+        
+        body = GetComponent<Rigidbody>();
+        
+    }
+
+    
+}
