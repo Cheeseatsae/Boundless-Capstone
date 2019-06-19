edc45a52a527d067e239cfcf7d09121ed9b87fdf
Russell <russell.ayres.90@gmail.com>
2019-06-19 12:16:33 +1000

    Health Component

---
+++ Assets/Scenes/AI Testing/GroundAI/AIBullet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class AIBullet : NetworkBehaviour
+{
+    public int damage = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>())
+        {
+            Health healthComp = other.GetComponent<Health>();
+            healthComp.CmdDoDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scenes/Components/Health.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    public int baseMaxHealth;
+    public int health;
+    [HideInInspector] public int maxHealth;
+    
+    public int baseHealthRegen;
+    public int healthRegen;
+    public float regenTick = 1;
+    
+    //Events
+    public delegate void TakeDamageDelegate(int amount);
+
+    [SyncEvent] 
+    public event TakeDamageDelegate EventTakeDamage;
+    
+    private void Start()
+    {
+        maxHealth = baseMaxHealth;
+        health = baseMaxHealth;
+        if (NetworkClient.active)
+        {
+            EventTakeDamage += TakeDamage;
+        }
+        if (healthRegen > 0)
+        {
+            StartCoroutine(Regen());
+        }       
+    }
+
+    [Command]
+    public void CmdDoDamage(int amount)
+    {
+        EventTakeDamage(amount);
+    }
+
+    private void TakeDamage(int amount)
+    {
+        health -= amount;
+    }
+
+    private IEnumerator Regen()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(regenTick);
+            health += healthRegen;
+            health = Mathf.Clamp(health, 0, maxHealth);
+            // send event
+        }
+    }
+}
