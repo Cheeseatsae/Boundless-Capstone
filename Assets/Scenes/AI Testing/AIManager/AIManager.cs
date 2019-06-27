3c92580e555998779a8d6599bdb4c7c50a7e5858
Kyllarr123 <russell.ayres.90@gmail.com>
2019-06-27 12:28:27 +1000

    RandomAI Spawn Location around the player

--- Assets/Scenes/AI Testing/AIManager/AIManager.cs
+++ Assets/Scenes/AI Testing/AIManager/AIManager.cs
@@ -3,12 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using Random = System.Random;
+
 
 public class AIManager : NetworkBehaviour
 {
      
     public List<GameObject> Players = new List<GameObject>();
 
+    public int maxEnemySpawnDistance;
+    public int minEnemySpawnDistance;
+
+    public GameObject groundAI;
+    
 
 
+    [Command]
+    public void CmdSpawnArounfPlayer()
+    {
+        foreach (GameObject player in Players)
+        {
+            float angle = UnityEngine.Random.Range(0.0f, Mathf.PI * 2);
+            
+            Vector3 dir = new Vector3(Mathf.Sin(angle),0,Mathf.Cos(angle));
+
+            dir *= UnityEngine.Random.Range(minEnemySpawnDistance, maxEnemySpawnDistance);
+
+            GameObject AI = Instantiate(groundAI, player.transform.position + dir, Quaternion.identity);
+            NetworkServer.Spawn(AI);
+
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CmdSpawnArounfPlayer();
+        }
+    }
 }
