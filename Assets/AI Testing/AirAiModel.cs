a331c837ec8f58c6b7216eb4f70bd27dc3d2739d
Kyllarr123 <russell.ayres.90@gmail.com>
2019-07-22 07:07:37 +1000

    Air Ai Beginnings

---
+++ Assets/AI Testing/AirAiModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class AirAiModel : AIBaseModel
+{
+    public GameObject target;
+    private float minDistance = Mathf.Infinity;
+    public AIManager aiManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiManager = FindObjectOfType<AIManager>();
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isServer) return;
+        foreach (GameObject player in aiManager.Players)
+        {
+            if (player != null)
+            {
+                float distance = Vector3.Distance(player.transform.position, transform.position);
+                if (distance < minDistance)
+                {
+                    target = player;
+                    minDistance = distance;
+                }
+            }
+            
+        }        
+    }
+}
