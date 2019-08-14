3a1079e9645b70525c3025b1fbb1afb753e6b410
Russell <russell.ayres.90@gmail.com>
2019-08-14 10:36:52 +1000

    Fix to ability 4

---
+++ Assets/Scenes/LobbyTest/LobbyUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUI : NetworkBehaviour
+{
+    public Slider killsSlider;
+
+    public Text killsText;
+    
+    public Slider aiSlider;
+
+    public Text aiText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if //(is)
+        {
+            killsSlider.enabled = true;
+            aiSlider.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //killsText.text = killsSlider.value.ToString();
+        //aiText.text = aiSlider.value.ToString();
+
+    }
+}
