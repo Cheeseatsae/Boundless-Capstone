382060906310c7d8195f05598bfd93d6d62dcbdf
Kyllarr123 <russell.ayres.90@gmail.com>
2019-12-06 00:00:50 +1100

    have another scene

--- Assets/UiManager.cs
+++ Assets/UiManager.cs
@@ -9,15 +9,24 @@ public class UiManager : MonoBehaviour
     public Animator settingsAnim;
     public GameObject interaction;
     public ItemLogger textLogger;
+
+    public GameObject youDied;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Health.PlayerDeath += Died;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Died()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        youDied.SetActive(true);
     }
 }
